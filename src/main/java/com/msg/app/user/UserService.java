72afbd038a4bb8679e2fad10763435f88d9a9db5
beomkim7 <beomkim7@naver.com>
2024-09-26 12:19:54 +0900

    join finish

--- src/main/java/com/msg/app/user/UserService.java
+++ src/main/java/com/msg/app/user/UserService.java
@@ -2,15 +2,18 @@ package com.msg.app.user;
 
 import com.msg.app.user.DTO.UserDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
     @Autowired
     private UserMapper userMapper;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     public int addUser(UserDTO userDTO) throws Exception {
-
+        userDTO.setPw(passwordEncoder.encode(userDTO.getPw()));
         return userMapper.addUser(userDTO);
     }
 }
